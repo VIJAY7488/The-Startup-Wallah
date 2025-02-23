a28701c679fe545d477599d953e9859104689ad2
Vijay Patel <vijaypatel3207@gmail.com>
2025-02-23 17:50:34 +0530

    About us section is done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './Hero'
+import AboutUs from './AboutUs'
 
 const Home = () => {
   return (
     <div className=''>
       <Hero />
+      <AboutUs />
     </div>
   )
 }
---
+++ src/pages/Investors.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Investors = () => {
+  return (
+    <div>
+      <h1>investors</h1>
+    </div>
+  )
+}
+
+export default Investors
