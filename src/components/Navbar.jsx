133791c4d1501d7fa8834c49750113e07f00206c
Vijay Patel <vijaypatel3207@gmail.com>
2025-02-23 16:10:15 +0530

    Navbar mobile responsive is done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
+import { RxHamburgerMenu } from "react-icons/rx";
+import { IoClose } from "react-icons/io5";
 
 const Navbar = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
   return (
     <>
       <nav 
@@ -33,17 +37,70 @@ const Navbar = () => {
 
 
                 <Link to="/signin">
-                  <button className="mr-1 border px-4 py-2 rounded hover:bg-blue-500 hover:text-white font-medium">
+                  <button 
+                    className="mr-1 border px-4 py-2 rounded hover:bg-blue-500 hover:text-white font-medium">
                     Sign In
                   </button>
                 </Link>
                 <Link to="/login">
-                  <button className='border px-4 py-2 rounded bg-blue-500 hover:bg-blue-600 text-white font-medium'>Login</button>
+                  <button 
+                    className='border px-4 py-2 rounded bg-blue-500 hover:bg-blue-600 text-white font-medium'>
+                    Login
+                  </button>
                 </Link>
+                </div>
 
-                    
+                {/* For mobile device */}
+
+                <div className='md:hidden'>
+                    <button 
+                        onClick={() => setIsOpen(!isOpen)}
+                        className="text-gray-600 hover:text-primary transition-colors" >
+                        {isOpen ? <IoClose className='text-2xl font-bold' /> : <RxHamburgerMenu className='text-2xl font-bold' />}
+                    </button>
                 </div>
             </div>
+
+            {isOpen && (
+                <div className="md:hidden" >
+                    <div 
+                        className="px-2 pt-2 pb-3 space-y-1 sm:px-3" >
+                        <Link 
+                            to='/investors'
+                            onClick={() => setIsOpen(false)}
+                            className='navbar-small-screen'>
+                            Investors
+                        </Link>
+                        <Link 
+                            to='/resources'
+                            onClick={() => setIsOpen(false)}
+                            className='navbar-small-screen'>
+                            Resources
+                        </Link>
+                        <Link 
+                            to='/services'
+                            onClick={() => setIsOpen(false)}
+                            className='navbar-small-screen'>
+                            Services
+                        </Link>
+
+                        <div className="mt-4 space-y-2 flex flex-col">
+                            <Link 
+                                to='/signin'
+                                onClick={() => setIsOpen(false)}
+                                className="w-full mb-3"> 
+                                <button className="w-full mr-1 border px-4 py-2 rounded hover:bg-blue-500 hover:text-white font-medium">Sign In</button>
+                            </Link>
+                            <Link 
+                                to='/login'
+                                onClick={() => setIsOpen(false)}
+                                className="w-full">
+                                <button className='w-full border px-4 py-2 rounded bg-blue-500 hover:bg-blue-600 text-white font-medium'>Login</button>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
       </nav>
     </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
 
 const Home = () => {
   return (
-    <div>
-      <Navbar/>
+    <div className='mt-16'>
+      <h1>Home</h1>
     </div>
   )
 }
